4961ae5b6013606833cd6f36fc212b78ba0dc6b5
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2024-07-11 08:33:52 +0200

    on peut se téléporter de coffin en coffin

--- Assets/Scripts/MapOverview.cs
+++ Assets/Scripts/MapOverview.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class MapOverview : MonoBehaviour
 {
+    public Camera minimapCamera; // La caméra qui projette la minimap
+    public Transform playerTransform; // Le transform du joueur
+    public LayerMask coffinLayerMask; // Le layer des cercueils
     private CanvasGroup canvasGroup;
+    
 
     void Awake()
     {
@@ -39,5 +43,36 @@ public class MapOverview : MonoBehaviour
             canvasGroup.blocksRaycasts = false;
             canvasGroup.interactable = false;
         }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity, coffinLayerMask);
+
+            if (hit.collider != null)
+            {
+                // Vérifiez si l'objet cliqué est un cercueil
+                if (hit.collider.CompareTag("Coffin"))
+                {
+                    TeleportToCoffin(hit.collider.transform);
+                }
+            }
+        }
+    }
+
+    void TeleportToCoffin(Transform coffinTransform)
+    {
+        if (playerTransform != null && coffinTransform != null)
+        {
+            playerTransform.position = coffinTransform.position;
+            Debug.Log("Joueur téléporté au cercueil : " + coffinTransform.name);
+        }
+        else
+        {
+            Debug.LogError("Référence manquante pour le joueur ou le cercueil.");
+        }
+        DeactivateOverview();
     }
 }
--- Assets/Scripts/TeleportationManager.cs
+++ Assets/Scripts/TeleportationManager.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class TeleportationManager : MonoBehaviour
 {
-    public Transform playerTransform;
+    public Camera minimapCamera; // La caméra qui projette la minimap
+    public Transform playerTransform; // Le transform du joueur
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            if (hit.collider != null)
+            {
+                // Vérifiez si l'objet cliqué est un cercueil
+                if (hit.collider.CompareTag("Coffin"))
+                {
+                    TeleportToCoffin(hit.collider.transform);
+                }
+            }
+        }
+    }
     public void TeleportToCoffin(Transform coffinTransform)
     {
+        Debug.Log("Téléportation");
         if (playerTransform != null && coffinTransform != null)
         {
             playerTransform.position = coffinTransform.position;
